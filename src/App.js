556ccee0e09573adac7964e088a459f687704003
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-11-22 16:51:26 -0300

    feat: api call and display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import './App.css';
+import Table from './components/Table';
+import AppContext from './context/AppContext';
 
 function App() {
+  const { fetchPlanets } = useContext(AppContext);
+  useEffect(() => {
+    fetchPlanets('https://swapi-trybe.herokuapp.com/api/planets/');
+  }, []);
   return (
-    <span>Hello, App!</span>
+    <div><Table /></div>
   );
 }
 
